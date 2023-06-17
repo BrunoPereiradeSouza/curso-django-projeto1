32a310122907a9edb1dc7f1e60dc1e5f466c2c8e
Bruno Souza <brunosouzaff10@gmail.com>
2023-06-17 15:01:52 -0300

    example

---
+++ recipes/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from recipes.views import home, contato, sobre
+
+
+
+urlpatterns = [
+    
+    path('', home, name="home"),
+    path('contato/', contato, name='contato'),
+    path('sobre/', sobre, name='sobre')
+]
+
+ 
